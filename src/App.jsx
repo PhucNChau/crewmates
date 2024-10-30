4f3ddac78d2efeb52a71f00a4dc332a1324c7c09
Phuc Chau <phucnchau@gmail.com>
2024-10-29 23:25:59 -0400

    Install router package. Add router to App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import { useState } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  
+  let router = createBrowserRouter([
+    {
+      path: "/",
+      // element: 
+    },
+    {
+      path: "/create",
+      // element: 
+    },
+    {
+      path: "/edit/:id",
+      // element: 
+    },
+    {
+      path: "/gallery",
+      // element: 
+    },
+  ]);
 
   return (
-    <div></div>
+    <RouterProvider router={router} />
   );
 };
 
